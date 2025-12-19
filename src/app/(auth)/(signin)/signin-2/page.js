008943467489a30e001cdda6e9a88ad912cb3685
faisalahmed3 <faisalahmed4417@gmail.com>
2025-12-19 22:04:12 +0600

    SignIn - 2 page Added

--- src/app/(auth)/(signin)/signin-2/page.js
+++ src/app/(auth)/(signin)/signin-2/page.js
@@ -1,7 +1,147 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
-export default function secondSignIn() {
+export const metadata = {
+  title: "Log in | Torkbiz",
+};
+
+export default function SecondSignInPage() {
   return (
-    <div>secondSignIn</div>
-  )
+    <section className="account padding-top padding-bottom bg-color-6">
+      <div className="container">
+        <div
+          className="account__wrapper"
+          data-aos="fade-up"
+          data-aos-duration="800"
+        >
+          <div className="account__inner">
+            <div className="row gx-4 align-items-center">
+
+              {/* LEFT IMAGE */}
+              <div className="col-lg-6">
+                <div className="account__thumb">
+                  <Image
+                    src="/images/account/2.png"
+                    alt="account image"
+                    width={600}
+                    height={600}
+                    priority
+                  />
+                </div>
+              </div>
+
+              {/* RIGHT CONTENT */}
+              <div className="col-lg-6">
+                <div className="account__content account__content--style2">
+
+                  {/* HEADER */}
+                  <div className="account__header">
+                    <h2>Log in</h2>
+                    <p className="mb-0">
+                      Hey there! Ready to log in? Just enter your credentials and
+                      we&apos;ll get you to your account in no time. Let&apos;s go!
+                    </p>
+                  </div>
+
+                  {/* SOCIAL */}
+                  <div className="account__social">
+                    <ul className="social">
+                      {[
+                        "facebook-f",
+                        "instagram",
+                        "linkedin-in",
+                        "youtube",
+                        "twitter",
+                      ].map((icon, i) => (
+                        <li className="social__item" key={i}>
+                          <a className="social__link social__link--style1">
+                            <i className={`fab fa-${icon}`} />
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+
+                  {/* DIVIDER */}
+                  <div className="account__divider account__divider--style2">
+                    <span>Or use you email to create account.</span>
+                  </div>
+
+                  {/* FORM */}
+                  <form className="account__form needs-validation">
+                    <div className="row g-4">
+
+                      <div className="col-12">
+                        <label className="form-label">Email</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          placeholder="Enter your email"
+                        />
+                      </div>
+
+                      <div className="col-12">
+                        <div className="form-pass">
+                          <label className="form-label">Password</label>
+                          <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Password"
+                          />
+                          <button
+                            type="button"
+                            className="form-pass__toggle"
+                          >
+                            <i className="fa fa-eye" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* REMEMBER + FORGOT */}
+                    <div className="account__check">
+                      <div className="account__check-remember">
+                        <input
+                          type="checkbox"
+                          className="form-check-input"
+                          id="remember-check"
+                        />
+                        <label
+                          htmlFor="remember-check"
+                          className="form-check-label"
+                        >
+                          Remember me
+                        </label>
+                      </div>
+
+                      <div className="account__check-forgot">
+                        <Link href="/forgot-pass-2">Forgot Password?</Link>
+                      </div>
+                    </div>
+
+                    <button
+                      type="submit"
+                      className="trk-btn trk-btn--border trk-btn--primary1 d-block mt-4"
+                    >
+                      Sign In
+                    </button>
+                  </form>
+
+                  {/* SWITCH */}
+                  <div className="account__switch">
+                    <p>
+                      Don&apos;t have an account?{" "}
+                      <Link href="/signup-2">Sign up</Link>
+                    </p>
+                  </div>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
